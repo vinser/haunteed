66a37b95ebacbe4c24103d32e5155ebc8e5bd02e
vinser <vinser@outlook.com>
2025-08-30 09:49:10 +0500

    Add MOTD and bosskey

--- internal/embeddata/data.go
+++ internal/embeddata/data.go
@@ -5,10 +5,10 @@ import (
 	"io/fs"
 )
 
-//go:embed about.md sounds.zip
+//go:embed about.md bosskey.json motd.json sounds.zip
 var embeddedFS embed.FS
 
-// FS returns the embedded filesystem with access to about.md amd sounds.zip.
+// FS returns the embedded filesystem with access to files in this folder.
 func FS() fs.FS {
 	return embeddedFS
 }
@@ -16,6 +16,16 @@ func FS() fs.FS {
 // ReadConfig returns the contents of about.md.
 func ReadAboutMD() ([]byte, error) {
 	return embeddedFS.ReadFile("about.md")
+}
+
+// ReadBoss returns the contents of bosskey.json.
+func ReadBoss() ([]byte, error) {
+	return embeddedFS.ReadFile("bosskey.json")
+}
+
+// ReadMOTD returns the contents of motd.json.
+func ReadMOTD() ([]byte, error) {
+	return embeddedFS.ReadFile("motd.json")
 }
 
 // ReadSoundsZip returns the contents of embedded sounds.zip.
